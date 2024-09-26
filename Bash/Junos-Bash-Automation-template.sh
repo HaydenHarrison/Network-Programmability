359dd6dbbb8c29513c07cf7060a62c9fee6f5be8
HaydenHarrison <HaydenHarrison175@gmail.com>
2024-09-26 08:03:22 -0500

    Add security measures for username and password verification

--- Bash/Junos-Bash-Automation-template.sh
+++ Bash/Junos-Bash-Automation-template.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 devices="SRX300"
+read -p "enter a username" username
+read -p "enter a password" password
 commands="edit; set interfaces ge-0/0/1 description To-PE ; show | compare; commit check; commit; exit; exit;"
 for i in ${devices}; do 
     echo "Working on device: ${i}"
-    sshpass -p "Juniper" ssh -o StrictHostKeyChecking=no admin@${i} ${commands}
+    sshpass -p ${password} ssh -o StrictHostKeyChecking=no ${username}@${i} ${commands}
     echo =======================
 done 
